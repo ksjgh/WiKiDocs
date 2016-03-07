2004d4a0becb93dbccb00c7707567876ccf77755
unknown <sj.kwon@HLBRD10-NA102MO.LGE.NET>
2016-03-07 14:46:40 +0900

    solution   answer : Sum of Multiple of 3 or 5 =233168

---
+++ Coding Dojang/Lv01_Multiples_of_3_and_5.py
@@ -0,0 +1,15 @@
+sum=0
+def MultipleOf_3(num):
+    if num%3==0:
+        return True
+    else:
+        return False
+
+def MultipleOf_5(num):
+    if num%5==0: return True
+    else: return False
+
+for i in range(1000):
+    if MultipleOf_3(i) or MultipleOf_5(i):
+        sum+=i
+print("Sum of Multiple of 3 or 5 ={0}".format(sum))
