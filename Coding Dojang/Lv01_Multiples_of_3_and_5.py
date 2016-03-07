b34df621cf55164dfee25b053b6b7ba2d7604421
unknown <sj.kwon@HLBRD10-NA102MO.LGE.NET>
2016-03-07 15:01:34 +0900

    add best answer

--- Coding Dojang/Lv01_Multiples_of_3_and_5.py
+++ Coding Dojang/Lv01_Multiples_of_3_and_5.py
@@ -1,3 +1,4 @@
+# My Code
 sum=0
 def MultipleOf_3(num):
     if num%3==0:
@@ -13,3 +14,6 @@ for i in range(1000):
     if MultipleOf_3(i) or MultipleOf_5(i):
         sum+=i
 print("Sum of Multiple of 3 or 5 ={0}".format(sum))
+
+# # Best Code
+# print(sum(list([x for x in range(1000) if x%3==0 or x%5==0 ])))
