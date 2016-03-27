22a63e240bca963de95479ecebd29fca1fc44856
JolinGom_Home <ksjksjgg@gmail.com>
2016-03-27 11:00:34 +0900

    upload

---
+++ 파이썬을 이용한 시스템 트레이딩 (기초편)/test.py
@@ -0,0 +1,35 @@
+#-*- coding:utf-8 -*-
+print("서현아")
+
+
+
+# f=open("C:/my temp/new_file.txt",'a')
+#
+# for i in range(11,20):
+#     data="%d added line\n" % i
+#     f.write(data)
+#
+# f.close
+
+
+# with open("C:/my temp/new_file3.txt",'w') as f:
+#     f.write("Life is too short, you need python")
+
+# import os
+#
+# print("Path at terminal when executing this file")
+# print(os.getcwd() + "\n")
+#
+# print("This file path, relative to os.getcwd()")
+# print(__file__ + "\n")
+#
+# print("This file full path (following symlinks)")
+# full_path = os.path.realpath(__file__)
+# print(full_path + "\n")
+#
+# print("This file directory and name")
+# path, filename = os.path.split(full_path)
+# print(path + ' --> ' + filename + "\n")
+#
+# print("This file directory only")
+# print(os.path.dirname(full_path))
