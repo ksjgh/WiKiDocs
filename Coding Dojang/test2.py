08b201dad751fd035fe3163d56a7951315ed21db
권성진/선임연구원〉MC 연구소 플랫폼실 플랫폼공용화개발팀 3파트(sj.kwon@lge.com) <sj.kwon@lge.com>
2016-05-24 11:48:20 +0900

    combination test code

--- Coding Dojang/test2.py
+++ Coding Dojang/test2.py
@@ -1,47 +1,27 @@
-import re
+# def facto(n):
+#     if n==1: return 1
+#     return n*facto(n-1)
+#
+# print(facto(6))
 
-# p=re.compile(r'\bclass')
-# m=p.search('aljfkjs         classajalkjdl')
-# print(m)
+# def arr_gen(n):
+#     arr=[]
+#     for i in range(n):
+#         arr.append(i)
+#         yield(arr)
+#
+# for x in arr_gen(10):
+#     print(x)
 
-# p = re.compile('(ABC)+')
-# m = p.search('ABCABCABC OK?')
-# print(m)
-# print(m.group())
-
-# p = re.compile(r"(\w+)\s+((\d+)[-]\d+[-]\d+)")
-# m = p.search("park 010-1234-1234 kim 010-8989-6363")
-# print(m.group(0))
-
-
-# p = re.compile(r'(\s+\w+\s+)')
-# m=p.search('Paris in the the spring')
-# print(m.group(0))
+def gen_combi(L,r):
+    if len(L)<=r or r<=0 : return L
+    combi_list=[]
+    for x in L:
+        combi_list.append(x)
+        L.remove(x)
+        combi_list.extend(gen_combi(L,r-1))
+        yield combi_list
 
 
-# p = re.compile(r'(\b\w+)\s+\1')
-# print(p.search('Paris in the the spring').group())
-
-# p = re.compile(r'(\b\w+\b)\s+\1')
-# print(p.search('Paris in the the spring').group())
-
-# p = re.compile(r'(?P<word>\b\w+)\s+(?P=word)')
-# print((p.search('Paris in the the spring')).group())
-
-# p = re.compile(r".+:")
-# m = p.search("http://google.com")
-# print(m.group())
-
-# regular expression , Lookahead Assertions, 전방탐색, forward search
-
-# p=re.compile(r'\w+')
-# m=p.search('A1 B2 c3 d_4 e:5 ffGG77--__--')
-# print(m)
-
-def hexrepl(match):
-     print(match.group())
-     value = int(match.group())
-     return hex(value)
-
-p = re.compile(r'\d+')
-print(p.sub(hexrepl, 'Call 65490 for printing, 49152 for user code.'))
+for i in gen_combi(list(range(5)),2):
+    print(i)
